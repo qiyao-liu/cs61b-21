026ae227752bfc6853e6273f181c1cc55a3c5b2e
Qiyao.Liu <Qiyao.Liu@671deMacBook-Pro.local>
2024-03-01 08:59:14 +0100

    collatz modified

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,7 +7,7 @@ public class Collatz {
     public static int nextNumber(int n) {
         if (n == 1) {
             return 1;
-        } else if (n % 2 != 1) {
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
             return n * 2;
